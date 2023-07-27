a25844cbc3775febbf41a0a48888e44de0224186
Abdelrauf <qwr@live.ru>
2023-07-27 10:04:52 +0000

    build and test

--- jupyterNotebooks/abdelrauf/mymodule/package1/numune.go
+++
@@ -1,5 +0,0 @@
-package package1
-  import "fmt"
-   func Salam(str string)  {
- 	fmt.Println( str )
-}
--- jupyterNotebooks/abdelrauf/mymodule/package1/numune2.go
+++
@@ -1,6 +0,0 @@
-package package1
-  import "github.com/common-nighthawk/go-figure"
-   func Salam2(str string)  {
- 	myFigure := figure.NewFigure(str, "", true) 
-  	myFigure.Print()
-}
